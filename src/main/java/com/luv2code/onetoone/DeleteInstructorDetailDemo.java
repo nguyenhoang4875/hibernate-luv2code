10856c84dcfe4466c613e6fa2775e2cba97604ff
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-01-04 21:32:20 +0700

    delete direction cascade hb-one-to-one binding

---
+++ src/main/java/com/luv2code/onetoone/DeleteInstructorDetailDemo.java
@@ -0,0 +1,60 @@
+package com.luv2code.onetoone;
+
+
+import com.luv2code.entity.Instructor;
+import com.luv2code.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructorDetailDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = sessionFactory.getCurrentSession();
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the instructor detail object
+            int theId = 3;
+            InstructorDetail instructorDetail = session.get(InstructorDetail.class,theId);
+
+            // print the instructor detail
+            System.out.println("instructorDetail: "+instructorDetail);
+
+            // print the associated instructor
+            System.out.println("the associated instructor: "+ instructorDetail.getInstructor());
+
+            // now let's delete the instructor detail
+            System.out.println("Deleting instructorDetail: "+ instructorDetail);
+            session.delete(instructorDetail);
+
+            // commit transaction
+
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+
+        }
+        finally {
+            // handle connection leak issue
+            session.close();
+            sessionFactory.close();
+        }
+
+    }
+}
