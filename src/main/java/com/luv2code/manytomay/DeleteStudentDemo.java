b381f5d0e6b31923a3bb32077a2d72de2019ee54
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-01-05 21:28:17 +0700

    Delete Student

---
+++ src/main/java/com/luv2code/manytomay/DeleteStudentDemo.java
@@ -0,0 +1,52 @@
+package com.luv2code.manytomay;
+
+
+import com.luv2code.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteStudentDemo {
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = sessionFactory.getCurrentSession();
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the student mary form database
+            int theId = 2;
+            Student student = session.get(Student.class,theId);
+
+            System.out.println("\nLoaded student: "+ student);
+            System.out.println("Courses: "+ student.getCourses());
+
+            // delete student
+            System.out.println("\nDeleting student: "+ student);
+            session.delete(student);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            // add clean up code
+            session.close();
+
+            sessionFactory.close();
+        }
+
+    }
+}
